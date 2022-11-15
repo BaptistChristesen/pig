da2112d602f910f45d5ecb2330e5a236ee5611bb
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2022-11-15 09:23:08 -0600

    Initial Commit

---
+++ pig/pigApp.swift
@@ -0,0 +1,17 @@
+//
+//  pigApp.swift
+//  pig
+//
+//  Created by caden christesen on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct pigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
